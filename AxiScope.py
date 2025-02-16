2dabd19e534daaec988f7fd93d8f2808d75cce88
N3MI-DG <129717180+N3MI-DG@users.noreply.github.com>
2025-02-17 10:38:58 +1100

    Grab tool data via js

--- AxiScope.py
+++ AxiScope.py
@@ -1,22 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from helpers.file import read_json_file
 import os
-# from waitress import serve
 
 import axiscope
 
-axiscope.app           = Flask(__name__)
-axiscope.settings      = read_json_file(os.path.join(os.getcwd(), "settings.json"))
+axiscope.app      = Flask(__name__)
+axiscope.settings = read_json_file(os.path.join(os.getcwd(), "settings.json"))
 
-
-import routes.index
-import routes.api
-import routes.tools
-
+@axiscope.app.route("/")
+def index():
+    return render_template(
+        "index.jinja",
+        title="AxiScope",
+        settings=axiscope.settings
+    )
 
 if __name__ == "__main__":
     axiscope.initialize()
-
-    
-    # serve(app, host='0.0.0.0', port=8080)
-    # app.run(host="0.0.0.0", port=5000, debug=True)
