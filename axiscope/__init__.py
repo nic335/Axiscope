32d11ddc7a97d2e7741669999793eaaea20a8e11
N3MI-DG <129717180+N3MI-DG@users.noreply.github.com>
2025-02-16 12:08:33 +1100

    Refactor movement buttons

--- axiscope/__init__.py
+++ axiscope/__init__.py
@@ -27,9 +27,11 @@ def initialize():
             return False
 
         app.jinja_env.globals.update(printer_url=printer_url)
+        app.jinja_env.globals.update(bounce_move_url=bounce_move_url)
+        app.jinja_env.globals.update(course_move_url=course_move_url)
 
         # serve(app, host='0.0.0.0', port=8080)
-        app.run(host="0.0.0.0", port=settings['port'], debug=True)
+        app.run(host=settings['host'], port=settings['port'], debug=True)
 
 
         __INITIALIZED__ = True
@@ -38,3 +40,31 @@ def initialize():
 
 def printer_url(path=""):
     return f"http://{settings['printer_url']}{path}"
+
+
+def bounce_move_url(axis, move, bounce_length=1, speed=500):
+    url = printer_url("/printer/gcode/script?script=")
+    url += "SAVE_GCODE_STATE NAME=bounce_move%0A"
+    url += "G91%0A"
+
+    if move < 0:
+        url += f"G0 {axis}-{(bounce_length+abs(move))} F{speed}%0A"
+        url += f"G0 {axis}{bounce_length} F{speed}%0A"
+    else:
+        url += f"G0 {axis}{bounce_length+move} F{speed}%0A"
+        url += f"G0 {axis}-{bounce_length} F{speed}%0A"
+
+    url += "RESTORE_GCODE_STATE NAME=bounce_move"
+
+    return url
+
+def course_move_url(axis, move):
+    url = printer_url("/printer/gcode/script?script=")
+    url += "SAVE_GCODE_STATE NAME=course_move%0A"
+    url += "G91%0A"
+    url += f"G0 {axis.upper()}{move} F{6000 if axis.upper() in ['X', 'Y'] else 1500}%0A"
+    url += "RESTORE_GCODE_STATE NAME=course_move"
+
+    return url
+
+
